50c045e217bed57c8358adca97ed50c0e6798526
Jérémy Cormier1 <jcormier1@dept-info.crosemont.quebec>
2023-10-31 09:12:10 -0400

    Création des fichiers de tests

---
+++ AllezHop/src/test/kotlin/com/example/allezhop/Controleurs/UtilisateurControleursTest.kt
@@ -0,0 +1,5 @@
+package com.example.allezhop.Controleurs
+
+class UtilisateurControleursTest {
+    //Christian a dit que c'était pas grave si on avait juste utilisateur controleur même si il y a des spécifications comme Passager et Conducteur
+}
