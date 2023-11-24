5546ef6d3f74aacc368f3be504f02bb80f53e2ef
MMane <mmane@dept-info.crosemont.quebec>
2023-11-24 11:14:33 -0500

    Ajout de données pour la table adresse

--- AllezHopBD_lmd.sql
+++ AllezHopBD_lmd.sql
@@ -14,3 +14,24 @@ INSERT INTO trajet (destination, position_départ, heure_arrivée, heure_départ
 VALUES
 ('6400 16e Avenue, Montréal, QC H1X 2S9', '9191 Cavendish Blvd, Saint-Laurent, Quebec H4T 1M8', '2023-01-19 08:15:00', '2023-01-19 07:40:00', 1),
 ('6400 16e Avenue, Montréal, QC H1X 2S9', '7275 Sherbrooke St E, Montreal, Quebec H1N 1E9', '2023-11-19 15:15:00', '2023-11-01 15:00:00', 2);
+
+INSERT INTO Adresse (numéro_municipal, rue, ville, état, code_postal, pays)
+VALUES
+('6510', '15e avenue', 'Montréal', 'QC', 'H1X2V5', 'CA'),
+('6400', '16e avenue', 'Montréal', 'QC', 'H1X2S9', 'CA'),
+('6578', '16e avenue', 'Montréal', 'QC', 'H1X2T2', 'CA'),
+('6399', '12e avenue', 'Montréal', 'QC', 'H1X3A6', 'CA'),
+('6537', '12e avenue', 'Montréal', 'QC', 'H1X3A8', 'CA'),
+('6092', '16e avenue', 'Montréal', 'QC', 'H1X2S8', 'CA'),
+('5705', '16e avenue', 'Montréal', 'QC', 'H1X2S7', 'CA'),
+('6421', 'boulevard Saint-Michel', 'Montréal', 'QC', 'H1Y2E9', 'CA'),
+('6310', 'boulevard Saint-Michel', 'Montréal', 'QC', 'H1Y2E7', 'CA'),
+('6195', '18e avenue', 'Montréal', 'QC', 'H1X2P8', 'CA');
+
+INSERT INTO Adresse (appartement, numéro_municipal, rue, ville, état, code_postal, pays)
+VALUES
+('12','6411', '20e avenue', 'Montréal', 'QC', 'H1X3P8', 'CA'),
+('22','6411', '20e avenue', 'Montréal', 'QC', 'H1X3P8', 'CA'),
+('10','6411', '20e avenue', 'Montréal', 'QC', 'H1X3P8', 'CA'),
+('21','6411', '20e avenue', 'Montréal', 'QC', 'H1X3P8', 'CA'),
+('33A','6411', '20e avenue', 'Montréal', 'QC', 'H1X3P8', 'CA');
